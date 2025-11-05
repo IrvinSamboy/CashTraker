bef710543fc6cfb9881b7af9f9448c28be1decc0
Irvin Samboy <contacoirvinsamboy@gmail.com>
2025-11-05 10:01:22 -0400

    Adding routes to create expense controller

--- src/routes/budgetRouter.ts
+++ src/routes/budgetRouter.ts
@@ -46,5 +46,19 @@ router.delete(
     BudgetController.delete
 )
 
+/** EXPENSE ROUTES */
+
+router.post(
+    "/:budgetId/expense", 
+    body("name")
+        .notEmpty().withMessage("Name cloud'nt be empty"),
+    body("amount")
+        .notEmpty().withMessage("Amount cloud'nt be empty")
+        .isNumeric().withMessage("Amount cloud be number")
+        .custom(value => value > 0).withMessage("Value cloud be greather than 0"),
+    handleInputErrors, 
+    BudgetController.create
+)
+
 
 export default router
