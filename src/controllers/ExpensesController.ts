080ae9a955372d75a1033bcfea619f6c74f592d1
Irvin Samboy <contacoirvinsamboy@gmail.com>
2025-11-05 09:34:45 -0400

    Creating expense controller

---
+++ src/controllers/ExpensesController.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from "express"
+import Expense from "../database/models/expenses"
+
+export class ExpenseController {
+    static create = async (req: Request, res: Response) => {
+        try {
+            const expense = new Expense(req.body)
+            expense.budgetId = req.budget.id
+
+            await expense.save()
+
+            return res.status(200).json({
+                message: expense
+            })
+        } catch (error) {
+            const errorMessage = (error as Error)
+            console.log(errorMessage.message)
+            return res.status(500).json({
+                message: "Internal server error"
+            })
+        }
+    }
+}
